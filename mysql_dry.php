543d87566143f603ab40df295a89836ff62ff7da
Andrew Churyumov <34455557+error0001@users.noreply.github.com>
2020-01-20 22:37:13 +0300

    Update mysql_dry.php

--- mysql_dry.php
+++ mysql_dry.php
@@ -9,8 +9,6 @@
 //    description TEXT
 // );
 
-// Хотим вставить вот такую строку:
-
 $data = array(
         "name" => "Cup",
         "price" => 5000,
@@ -18,4 +16,31 @@ $data = array(
     );
    
 echo mysql_write_row('goods', $data); // return 1
+
+// 2. Обновлять строку
+$set_keys = array(
+    'f1'=>11,
+    'f2'=>22
+);
+$where_keys = array(
+    'f2'=>2,
+    'f3'=>3
+);
+mysql_write_row('tablename', $set_keys, $where_keys);
+// UPDATE tablename SET f1=11, f2=22 WHERE f2=2 AND f3=3
+
+// 3. Экранирование escape последовательностей 
+$id = 5;
+$name = "O'Reilly";
+
+$sql = "
+    SELECT ...
+    WHERE id = " . mysql_escape($id) . "
+      AND name = " . mysql_escape($name) . ";
+    ";
+// В результате получится запрос
+// SELECT ...
+// WHERE id = 5
+//   AND name = 'O\'Reilly'
+
 ?>
