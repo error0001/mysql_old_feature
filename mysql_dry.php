d46bcb6e3783b0802cb371cceb16fe7e0d292262
Andrew Churyumov <34455557+error0001@users.noreply.github.com>
2020-01-20 22:21:20 +0300

    Create mysql_dry.php

---
+++ mysql_dry.php
@@ -0,0 +1,20 @@
+<?php
+
+<?php
+// CREATE TABLE goods (
+//  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//    name VARCHAR(255),
+//    price DECIMAL(5),
+//    description TEXT
+// );
+
+// Хотим вставить вот такую строку:
+
+$data = array(
+        "name" => "Cup",
+        "price" => 5000,
+        "description" => "Good reach cup."
+    );
+   
+echo mysql_write_row('goods', $data); // return 1
+?>
